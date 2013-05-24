be88ee93891a64a3e8aa904f7623f781c3ea8dfd
dmitry.shesterkin <dfb@yandex.ru>
2013-05-24 14:41:07 +0400

    MacOS Bundle generation

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,15 @@
 #include "Main.h"
 
 #include "System/App.h"
+#include <boost/filesystem.hpp>
 
 int SDL_main(int argc, char *argv[])
 {
+  using namespace boost::filesystem;
+  
   LOG( FMT("Start path: %s") % argv[0] );
+  current_path( system_complete(argv[0]).parent_path() );
+  
   App theApp;
 
   return theApp.Execute( Point(755, 600) );
